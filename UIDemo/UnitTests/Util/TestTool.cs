7b19ef02fb2282cfb697f4519c414570abb440b9
Grant Birchmeier <gbirchmeier@connamara.com>
2011-10-20 16:07:15 -0500

    refactor UT framework a little

--- UIDemo/UnitTests/Util/TestTool.cs
+++ UIDemo/UnitTests/Util/TestTool.cs
@@ -8,9 +8,38 @@ namespace UnitTests.Util
 {
     public static class TestTool
     {
-        static public void SetupAppAndSession(QFApp app, SessionThatTracksOutbound session)
+        private static QuickFix.Dictionary CreateConfig()
         {
-            return;
+            QuickFix.Dictionary config = new QuickFix.Dictionary();
+            config.SetBool(QuickFix.SessionSettings.PERSIST_MESSAGES, false);
+            config.SetString(QuickFix.SessionSettings.CONNECTION_TYPE, "initiator");
+            config.SetString(QuickFix.SessionSettings.START_TIME, "00:00:00");
+            config.SetString(QuickFix.SessionSettings.END_TIME, "00:00:00");
+            return config;
+        }
+
+        static public void SetupAppAndSession(out QFApp app, out SessionThatTracksOutbound session, out MockInitiator init)
+        {
+            QuickFix.Dictionary config = CreateConfig();
+            QuickFix.SessionID sessionID = new QuickFix.SessionID("FIX.4.2", "SENDER", "TARGET");
+            QuickFix.SessionSettings settings = new QuickFix.SessionSettings(); //CreateSettings(sessionID);
+            settings.Set(sessionID, config);
+
+            app = new QFApp(settings);
+            init = new MockInitiator();
+            app.Initiator = init;
+
+            session = new SessionThatTracksOutbound(
+                app,
+                new QuickFix.MemoryStoreFactory(),
+                sessionID,
+                new QuickFix.DataDictionaryProvider(),
+                new QuickFix.SessionSchedule(config),
+                0,
+                new QuickFix.ScreenLogFactory(settings),
+                new QuickFix.DefaultMessageFactory(),
+                "blah");
+            session.MaxLatency = 120;
         }
     }
 }
