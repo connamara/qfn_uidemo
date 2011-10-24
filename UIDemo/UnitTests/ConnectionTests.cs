1edfc255124564df3632880914028e53af161cac
Grant Birchmeier <gbirchmeier@connamara.com>
2011-10-23 19:01:03 -0500

    connection tests!

---
+++ UIDemo/UnitTests/ConnectionTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using UIDemo.ViewModel;
+using UnitTests.Util;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class ConnectionTests
+    {
+        [Test]
+        public void StartupStateTest()
+        {
+            UnitTestContext context = new UnitTestContext();
+            ConnectionViewModel vm = new ConnectionViewModel(context.App);
+
+            Assert.AreEqual("FIX.4.2:SENDER->TARGET", vm.SessionString);
+            Assert.False(vm.IsConnected);
+        }
+
+        [Test]
+        public void ConnectAndDisconnect()
+        {
+            UnitTestContext context = new UnitTestContext();
+            ConnectionViewModel vm = new ConnectionViewModel(context.App);
+
+            Assert.False(vm.IsConnected);
+
+            vm.ConnectCommand.Execute(null);
+            context.Login();
+            Assert.True(vm.IsConnected);
+
+            vm.DisconnectCommand.Execute(null);
+            context.Logout();
+            Assert.False(vm.IsConnected);
+        }
+    }
+}
