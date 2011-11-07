c19d577602420dfeb20a040b4567387ca813ced5
Grant Birchmeier <gbirchmeier@connamara.com>
2011-11-07 13:04:50 -0600

    refactor so I can make another build that provides an alternate strategy

--- UIDemo/UIDemo/App.xaml.cs
+++ UIDemo/UIDemo/App.xaml.cs
@@ -1,15 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Windows;
-using QuickFix;
-using UIDemo.View;
-using UIDemo.ViewModel;
-using System.Threading;
-using FIXApplication;
 
 namespace UIDemo
 {
@@ -18,70 +11,27 @@ namespace UIDemo
     /// </summary>
     public partial class App : System.Windows.Application
     {
-        QFApp _qfapp = null;
+        AppContext _appContext = null;
 
         public App()
             : base()
-        { }
-
-        /// <summary>
-        /// Virtual in case you want to derive this class and pass an ICustomFixStrategy to QFApp
-        /// </summary>
-        /// <param name="settings"></param>
-        /// <returns>a new QFApp object</returns>
-        protected virtual QFApp CreateQFApp(QuickFix.SessionSettings settings)
         {
-            return new QFApp(settings);
+            _appContext = new AppContext();
         }
 
         private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
-            Trace.WriteLine("Uncaught exception:");
-            Trace.WriteLine(e.Exception.ToString());
+            _appContext.Application_DispatcherUnhandledException(sender, e);
         }
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            Trace.WriteLine("Application started.");
-
-            // FIX application setup
-            QuickFix.SessionSettings settings = new QuickFix.SessionSettings("quickfix.cfg");
-            QuickFix.MessageStoreFactory storeFactory = new QuickFix.FileStoreFactory(settings);
-            QuickFix.LogFactory logFactory = new QuickFix.ScreenLogFactory(settings);
-
-            _qfapp = CreateQFApp(settings);
-
-            QuickFix.IInitiator initiator = new QuickFix.Transport.SocketInitiator(_qfapp, storeFactory, settings, logFactory);
-            _qfapp.Initiator = initiator;
-
-
-            // Window creation and context assignment
-            MainWindow mainWindow = new MainWindow();
-            mainWindow.DataContext = new MainWindowViewModel();
-
-            mainWindow.MessageView.DataContext = new MessageViewModel(_qfapp);
-            mainWindow.ExecutionView.DataContext = new ExecutionViewModel(_qfapp);
-            mainWindow.ConnectionView.DataContext = new ConnectionViewModel(_qfapp);
-            mainWindow.OrderView.DataContext = new OrderViewModel(_qfapp);
-            mainWindow.NewsSenderView.DataContext = new NewsSenderViewModel(_qfapp);
-
-            // Set the main UI dispatcher
-            SmartDispatcher.SetDispatcher(mainWindow.Dispatcher);
-
-            mainWindow.Show();
+            _appContext.Application_Startup(sender, e, new FIXApplication.NullFixStrategy());
         }
 
         private void Application_Exit(object sender, ExitEventArgs e)
         {
-            try
-            {
-                Trace.WriteLine("Application exit.");
-                _qfapp.Stop();
-            }
-            catch (Exception ex)
-            {
-                ex.ToString();
-            }
+            _appContext.Application_Exit(sender, e);
         }
     }
 }
---
+++ UIDemo/UIDemo/AppContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FIXApplication;
+using System.Diagnostics;
+using System.Windows;
+using UIDemo.ViewModel;
+
+namespace UIDemo
+{
+    public class AppContext
+    {
+        QFApp _qfapp = null;
+
+        public void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            Trace.WriteLine("Uncaught exception:");
+            Trace.WriteLine(e.Exception.ToString());
+        }
+
+        public void Application_Startup(
+            object sender,
+            StartupEventArgs e,
+            ICustomFixStrategy strategy)
+        {
+            Trace.WriteLine("Application started.");
+
+            // FIX application setup
+            QuickFix.SessionSettings settings = new QuickFix.SessionSettings("quickfix.cfg");
+            QuickFix.MessageStoreFactory storeFactory = new QuickFix.FileStoreFactory(settings);
+            QuickFix.LogFactory logFactory = new QuickFix.ScreenLogFactory(settings);
+
+            _qfapp = new QFApp(settings, strategy);
+
+            QuickFix.IInitiator initiator = new QuickFix.Transport.SocketInitiator(_qfapp, storeFactory, settings, logFactory);
+            _qfapp.Initiator = initiator;
+
+
+            // Window creation and context assignment
+            UIDemo.MainWindow mainWindow = new UIDemo.MainWindow();
+            mainWindow.DataContext = new MainWindowViewModel();
+
+            mainWindow.MessageView.DataContext = new MessageViewModel(_qfapp);
+            mainWindow.ExecutionView.DataContext = new ExecutionViewModel(_qfapp);
+            mainWindow.ConnectionView.DataContext = new ConnectionViewModel(_qfapp);
+            mainWindow.OrderView.DataContext = new OrderViewModel(_qfapp);
+            mainWindow.NewsSenderView.DataContext = new NewsSenderViewModel(_qfapp);
+
+            // Set the main UI dispatcher
+            SmartDispatcher.SetDispatcher(mainWindow.Dispatcher);
+
+            mainWindow.Show();
+        }
+
+        public void Application_Exit(object sender, ExitEventArgs e)
+        {
+            try
+            {
+                Trace.WriteLine("Application exit.");
+                _qfapp.Stop();
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+        }
+    }
+}
